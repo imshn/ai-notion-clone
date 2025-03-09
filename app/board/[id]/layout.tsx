ab45859e96b928b6a4d5a1ad7b9d7c08967402e4
imshn <shaan@vyomr.in>
2025-03-09 18:45:11 +0530

    prod error

--- app/board/[id]/layout.tsx
+++ app/board/[id]/layout.tsx
@@ -1,9 +1,19 @@
+"use client"
 import RoomProvider from '@/components/RoomProvider'
 
-import React from 'react'
-const BoardLayout = ({ children, params: { id } }: { children: React.ReactNode, params: { id: string } }) => {
+import React, { useEffect, useState } from 'react'
+const BoardLayout = ({ children, params: { id } }: { children: React.ReactNode, params: { id: Promise<string> } }) => {
+    const [roomId, setRoomId] = useState<string>()
+    const returnId = async () => {
+        const resolvedId = await id
+        setRoomId(resolvedId)
+    }
+    useEffect(() => {
+        returnId()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
     return (
-        <RoomProvider roomId={id} >{children}</RoomProvider>
+        <RoomProvider roomId={roomId as string} >{children}</RoomProvider>
     )
 }
 
