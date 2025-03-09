bab1fc2521a3220cba40261c947373c321e9bc24
imshn <shaan@vyomr.in>
2025-03-09 18:35:23 +0530

    ..

--- app/board/[id]/layout.tsx
+++ app/board/[id]/layout.tsx
@@ -1,8 +1,6 @@
 import RoomProvider from '@/components/RoomProvider'
 
 import React from 'react'
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const BoardLayout = ({ children, params: { id } }: { children: React.ReactNode, params: { id: string } }) => {
     return (
         <RoomProvider roomId={id} >{children}</RoomProvider>
